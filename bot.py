3e3245371348607c04687411033256e66f80fe18
trollhole666 <148247472+trollhole666@users.noreply.github.com>
2025-08-05 22:48:00 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,8 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.types import WebAppInfo
 from aiogram.utils import executor
 
-# ⚠️ Используй токен только локально. ОБЯЗАТЕЛЬНО поменяй его позже через BotFather
-API_TOKEN = '7926988231:AAGlqZNcBm-q3hd5KHBdHMOn-H5_-xwet5w'
+API_TOKEN = '7926988231:AAGlqZNcBm-q3hd5KHBdHMOn-H5_-xwet5w'  # поменяй позже
 
-# Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
@@ -15,7 +13,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     keyboard = types.InlineKeyboardMarkup()
-    web_app = WebAppInfo(url='https://zalupa-8ki2.onrender.com')  # адрес твоей игры
+    web_app = WebAppInfo(url="https://zalupa-8ki2.onrender.com")  # адрес твоей игры
     keyboard.add(types.InlineKeyboardButton(text="🪳 Играть в Тараканов", web_app=web_app))
     await message.answer("👋 Привет! Готов раздавить тараканов?", reply_markup=keyboard)
 
